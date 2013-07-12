095b59febb759ba8897a9dd2c047e39a343845a5
Japa <japa.mala.illo@gmail.com>
2013-07-12 12:47:04 +0530

    Create new project.

---
+++ Shadowbot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Shadowbot
+{
+    struct IRCConfig
+    {
+        public string server;
+        public int port;
+        public string nick;
+        public string name;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
